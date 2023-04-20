bc563d012184cd1773574eee5b7fca2c202dc5be
Nicholas Nguyen <NicholasNguyen8742@gmail.com>
2023-04-19 22:51:52 -0500

    Styling w/ Flex Box on Blog Post Cards

--- my-app/src/components/BlogPostCard/index.tsx
+++ my-app/src/components/BlogPostCard/index.tsx
@@ -9,18 +9,18 @@ const BlogPostCard = ({featureImage, title, id, date, description,author, featur
         <div style={{ width: '100%' }}>
           <img src={featureImage} alt={featureImageAlt} style={{ width: '100%', display: 'block', objectFit: 'cover' }} />
         </div>
-        <div style={{ padding: '1rem' }}>
+        <div style={{ padding: '1rem', display: 'flex', flexDirection: 'column', height: '100%' }}>
           <BlogCardTitle>
             <Link to={`/blog/${id}`}>{title}</Link>
           </BlogCardTitle>
           <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
             {description}
           </p>
-          <div style={{ display: 'flex', justifyContent: 'space-between'}}>
-            <p>{author}</p>
-            <p>{date}</p>
-          </div>
-          <div>
+          <div style={{ display: 'flex', flexDirection: 'column', marginTop: 'auto', height: 'fit-content', }}>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', }}>
+              <p style={{ fontSize: '1rem', lineHeight: '1.25rem' }}>{author}</p>
+              <p style={{ fontSize: '1rem', lineHeight: '1.25rem' }}>{date}</p>
+            </div>
             <BlogCardNavLink to={`/blog/${id}`}>
               Continue Reading
             </BlogCardNavLink>
